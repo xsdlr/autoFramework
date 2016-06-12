64a6471dec3dd2a1151001aedfae4e415dc187f8
消失的旅人 <869412057@qq.com>
2016-06-12 12:14:54 +0800

    修改脚本

--- README.md
+++ README.md
@@ -3,13 +3,15 @@
 
 # Usage
 
-     -t --target 工程名
+     -t --target  工程名
+     -p --project 项目名
    
      1 cd进入工程文件夹
-     2 $ python autoFramework.py -t xxx (工程名)
+     2 $ python autoFramework.py -p xxx (项目名) -t xxx (工程名)
        
        出现下面的打印，代表输出成功，Output即为输出的路径，即工程文件夹下。
       -------------RESULT----------------
       Success
       Output: /Users/chiyou/Desktop/Framework/Framework.framework
      
+
--- autoFramework.py
+++ autoFramework.py
@@ -7,7 +7,7 @@ import requests
 CONFIGURATION = "Release"
 
 
-def buildProject(target):
+def buildProject(project, target):
 
     process = subprocess.Popen("pwd", stdout=subprocess.PIPE)
     (stdoutdata, stderrdata) = process.communicate()
@@ -19,10 +19,10 @@ def buildProject(target):
     print (simulatorLibPath)
 
     process.wait()
-    buildOSCmd = 'xcodebuild -project %s.xcodeproj -target %s -sdk iphoneos -configuration %s ONLY_ACTIVE_ARCH="NO" ARCHS="armv7 armv7s arm64" VALID_ARCHS="armv7 armv7s arm64"' %(target,target,CONFIGURATION)
+    buildOSCmd = 'xcodebuild -project %s.xcodeproj -target %s -sdk iphoneos -configuration %s clean build' %(project,target,CONFIGURATION)
     process = subprocess.Popen(buildOSCmd, shell=True)
     process.wait()
-    buildSimulatorCmd = 'xcodebuild -project %s.xcodeproj -target %s -sdk iphonesimulator -configuration %s ONLY_ACTIVE_ARCH="NO"' %(target,target,CONFIGURATION)
+    buildSimulatorCmd = 'xcodebuild -project %s.xcodeproj -target %s -sdk iphonesimulator -configuration %s clean build' %(project,target,CONFIGURATION)
     process = subprocess.Popen(buildSimulatorCmd, shell=True)
     process.wait()
     cpCmd = 'cp -r %s/Release-iphoneos/%s.framework %s' %(buildDir,target,stdoutdata.strip())
@@ -45,23 +45,25 @@ def buildProject(target):
         print '\n'
 
 def xcbuild(options):
-	target = options.target
+    project = options.project
+    target = options.target
 
-	if target is None:
-		pass
-	elif target is not None:
-		buildProject(target)
+    if project is None or target is None:
+        pass
+    else:
+        buildProject(project, target)
 
 def main():
-	
-	parser = OptionParser()
-	parser.add_option("-t", "--target", help="Build the target specified by targetname. Required if building a project.", metavar="targetname")
+    
+    parser = OptionParser()
+    parser.add_option("-p", "--project", help="Build of the projectname. Required if building a project.", metavar="projectname")
+    parser.add_option("-t", "--target", help="Build the target specified by targetname. Required if building a project.", metavar="targetname")
 
-	(options, args) = parser.parse_args()
+    (options, args) = parser.parse_args()
 
-	print "options: %s, args: %s" % (options, args)
+    print "options: %s, args: %s" % (options, args)
 
-	xcbuild(options)
+    xcbuild(options)
 
 if __name__ == '__main__':
-	main()
+    main()
